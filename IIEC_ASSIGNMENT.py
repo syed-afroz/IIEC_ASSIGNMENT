3795a505ee8dc1c78a1c33b0f24d904c1eb4449b
syed-afroz <64829986+syed-afroz@users.noreply.github.com>
2020-08-22 10:47:52 +0530

    Add files via upload

---
+++ IIEC_ASSIGNMENT.py
@@ -0,0 +1,109 @@
+import os
+syed_system = input("Enter Name Of The Application:")
+if("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("chrome" in syed_system ):
+    os.system("chrome")
+    if("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("chrome youtube" in syed_system or  "youtube chrome" in syed_system ):
+        os.system("chrome youtube.com")
+    elif ("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("chrome uoh"in syed_system or "chrome university of hyderabad" in syed_system or  "chrome central university of hyderabad" in syed_system):
+        os.system("chrome uohyd.ac.in")
+
+
+#ms office
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("word" in syed_system or  "msword" in syed_system or "winword" in syed_system):
+    os.system("start winword")
+
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("excel" in syed_system or "msexcel" in syed_system):
+    os.system("start excel")
+
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("powerpoint" in syed_system or "mspowerpoint" in syed_system or "powerpnt" in syed_system):
+    os.system("start powerpnt")
+
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("access" in syed_system or "msaccess" in syed_system):
+    os.system("start msaccess")
+
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("mspublisher" in syed_system or "publisher"in syed_system or "mspub" in syed_system):
+    os.system("start mspub")
+
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("onenote" in syed_system or "msonenote" in syed_system):
+    os.system("start onenote")
+
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("outlook" in syed_system or "msoutlook" in syed_system):
+    os.system("start outlook")
+# URI COMMANDS
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("3D Builder" in syed_system or "Builder" in syed_system or "3D builder" in syed_system or "builder" in syed_system):
+    os.system("start com.microsoft.builder3d:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Alarms & Clock" in syed_system or "Alaram" in syed_system or "clock" in syed_system or "alaram" in syed_system or "Clock" in syed_system ): #captalization is importana
+    os.system("start ms-clock:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("calci" in syed_system or "calculator" in syed_system or "calc" in syed_system ): #captalization is importana
+    os.system("calc")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Calendar" in syed_system or "calender" in syed_system): #captalization is importana
+    os.system("start outlookcal:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Camera" in syed_system or "camera" in syed_system):
+    os.system("start microsoft.windows.camera:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Connect" in syed_system or "connect" in syed_system or "projection" in syed_system):
+    os.system("start ms-projection:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Cortana" in syed_system or "cortana" in syed_system):
+    os.system("start ms-cortana:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Feedback Hub" in syed_system or "feedback hub" in syed_system):
+    os.system("start feedback-hub:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Get Help" in syed_system or "get help" in syed_system):
+    os.system("start ms-contact-support:")
+#github
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("git hub" in syed_system or "Git Hub" in syed_system or "git" in syed_system or "Git" in syed_system):
+    os.system("github")
+#URI COMMANDS
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Mail" in syed_system or "mail" in syed_system):
+    os.system("start outlookmail:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Maps" in syed_system or "maps" in syed_system):
+    os.system("start bingmaps:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Messaging" in syed_system or "messaging" in syed_system or "chat" in syed_system or "Chat" in syed_system):
+    os.system("start ms-chat:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Microsoft News" in syed_system or "microsoft news" in syed_system or "MicrosoftNews" in syed_system or "microsoftnews" in syed_system or "news" in syed_system):
+    os.system("start bingnews:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Microsoft Solitaire Collection" in syed_system or "MicrosoftSolitaireCollection" in syed_system or "microsoft solitaire collection" in syed_system or "microsoftsolitairecollection" in syed_system or "solitaire" in syed_system or "Solitaire" in syed_system):
+    os.system("start xboxliveapp-1297287741:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Microsoft Store" in syed_system or "microsoft store" in syed_system or "MicrosoftStore" in syed_system or "microsoftstore" in syed_system or "store" in syed_system):
+    os.system("start ms-windows-store:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Paint 3D" in syed_system or "paint 3D" in syed_system or "paint" in syed_system or "Paint" in syed_system):
+    os.system("start ms-paint:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("people" in syed_system or  "People" in syed_system):
+    os.system("start ms-people:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("photos" in syed_system or  "Photos" in syed_system):
+    os.system("start ms-photos:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Screen Snip" in syed_system or  "screen snip" in syed_system or "ScreenSnip" in syed_system or  "screensnip" in syed_system):
+    os.system("start ms-screenclip:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Settings" in syed_system or  "settings" in syed_system):
+    os.system("start ms-settings:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Tips" in syed_system or  "tips" in syed_system):
+    os.system("start ms-get-started:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Voice Recorder" in syed_system or  "VoiceRecorder" in syed_system or "voice recorder" in syed_system or  "voicerecorder" in syed_system or "recoder" in syed_system or "Recorder" in syed_system):
+    os.system("start ms-callrecording:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Weather" in syed_system or  "weather" in syed_system):
+    os.system("start bingweather:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Windows Parental Controls" in syed_system or  "WindowsParentalControls" in syed_system or "windows parental controls" in syed_system or  "windowsparentalcontrols" in syed_system or "parentalcontrols" in syed_system or "ParentalControls" in syed_system or "wpc" in syed_system or "WPC" in syed_system):
+    os.system("start ms-wpc:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Windows Security" in syed_system or  "windowssecurity" in syed_system or "WindowsSecurity" in syed_system or  "windows security" in syed_system or "recoder" in syed_system or "Recorder" in syed_system or "Security" in syed_system or "security" in syed_system):
+    os.system("start windowsdefender:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("Xbox" in syed_system or  "xbox" in syed_system):
+    os.system("start xbox:")
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("wmplayer"in syed_system or "windowsmediaplayer" in syed_system or "mediaplayer" in syed_system):
+    os.system("wmplayer")
+elif("run" or "open" or "execute" in syed_system) and ("vlc" in syed_system or "vlcmediaplayer" in syed_system  or "vlcplayer"in syed_system):
+    os.system("vlc")
+#TEXT EDITORD
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("atom" in syed_system):
+    os.system("atom")
+
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("notepad++" in syed_system):
+    os.system("notepad++")
+
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("notepad" in syed_system):
+    os.system("notepad")
+
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("pycharm" in syed_system):
+    os.system("pycharm")
+#sublimetext3
+elif("run" in syed_system or "open" in syed_system or "execute" in syed_system) and ("sublime" in syed_system or "text3" in syed_system or "sublimetext3" in syed_system or "subl" in syed_system):
+    os.system("subl")
+else:
+    print("sorry couldn't find the file")
